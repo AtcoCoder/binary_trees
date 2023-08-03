89756c6e96e3f6c5f3c30a423a31db939b754fc9
Omar Jammeh <orms45@gmail.com>
2023-08-03 14:32:14 +0000

    Updated depth checker function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,6 +2,23 @@
 
 
 /**
+ * _binary_tree_depth - Measures the depth of a node in a binary tree
+ *
+ * @tree: A pointer to the node to measure the depth
+ *
+ * Return: The depth of a node in a binary tree.
+ */
+size_t _binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+	return (_binary_tree_depth(tree->parent) + 1);
+}
+
+
+/**
  * binary_tree_depth - Measures the depth of a node in a binary tree
  *
  * @tree: A pointer to the node to measure the depth
@@ -12,7 +29,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
-	return (binary_tree_depth(tree->parent) + 1);
+	return (_binary_tree_depth(tree));
 }
