90ec6f399bb967d66a4b6296319d5b24bf0d5ac5
Omar Jammeh <orms45@gmail.com>
2023-08-03 14:43:36 +0000

    Correction: the the height function for balance factor function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -38,25 +38,10 @@ size_t _binary_tree_height(const binary_tree_t *tree)
 		return (-1);
 	}
 	left_height = _binary_tree_height(tree->left);
-	right_height = _binary_tree_height(tree->right)
+	right_height = _binary_tree_height(tree->right);
 	return (max(left_height, right_height) + 1);
 }
 
-/**
- * binary_tree_height - measures the height of a binary tree
- *
- * @tree: A pointer to the root node of the tree to measure the height
- *
- * Return: The height of a binary tree, 0 if tree is empty.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	return (_binary_tree_height(tree));
-}
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
@@ -75,9 +60,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	printf("Left height is: %d\n", left_height);
-	right_height = binary_tree_height(tree->right);
-	printf("right height is: %d\n", right_height);
+	left_height = _binary_tree_height(tree->left);
+	right_height = _binary_tree_height(tree->right);
 	return (left_height - right_height);
 }
